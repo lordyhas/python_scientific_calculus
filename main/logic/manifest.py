7577cec9ef3ec55e6bcd1bde564ec3023b78a113
lordyhas <dev.haspro@gmail.com>
2022-12-08 12:03:36 +0100

    version 21.4+3

--- main/logic/manifest.py
+++ main/logic/manifest.py
@@ -1,7 +1,10 @@
 
 class App:
     version = 21.4
+    build_number = 3
     name = "L2CS"
     full_name = "L2 Calcul Scientifique"
 
 
+class PyLang :
+    version = ".".join([str(sys.version_info[0]),str(sys.version_info[1])])
